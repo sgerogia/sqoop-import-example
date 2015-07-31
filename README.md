357d9aa384133740c6b03d2696c99c4d26124005
Stelios Gerogiannakis <Stelios.Gerogiannakis@rbs.com>
2015-07-31 17:20:01 +0100

    Support for data types including BLOBs

--- README.md
+++ README.md
@@ -15,6 +15,39 @@ Commented and blank lines are ignored.
 * It lets Sqoop take care of import to Hive 
 * Tables are processed one at a time and any errors are printed on the console.
 
+### Data type mapping
+
+Use this section if you need to process a specific DB column which is not recognized by default. 
+
+You can add multiple DB column names as a comma-separated list, in the form `columnName=DateType`.
+
+The possible datatypes you can use are:
+
+* `Integer`
+* `Long`
+* `Float`
+* `Double`
+* `Boolean`
+* `String`
+* `java.sql.Date`
+* `java.sql.Time`
+* `java.sql.Timestamp`
+* `java.math.BigDecimal`
+* `com.cloudera.sqoop.lib.ClobRef` (for `CLOB` DB columns, will become a Hive `BINARY` column)
+* `com.cloudera.sqoop.lib.BlobRef` (for `CLOB` DB columns, will become a Hive `BINARY` column)
+
+**Important:** Only use this functionality if a column is causing issues or you want to optimize some downstream process.
+ In most cases, Hive's default translation is the optimal.
+
+### LOB 
+ 
+Large objects are always imported as non-materialized objects, i.e. the data is stored in a separate sub-folder.
+ 
+See 
+ 
+* Hive's [Large Objects][5] documentation 
+* Detailed explanation in [Hadoop: The Definitive Guide][6]
+
 ## Configuration 
 
 Before you can use the script on your environment, make sure that 
@@ -53,3 +86,5 @@ You may want to consider the following as possible improvements (or as exercise)
    [2]: ./scripts/sqoop-import.sh
    [3]: ./scripts/import.sh
    [4]: ./docs/res/linefeed.png?raw
+   [5]: https://sqoop.apache.org/docs/1.4.2/SqoopUserGuide.html#_large_objects
+   [6]: https://books.google.co.uk/books?id=MhqkBwAAQBAJ&pg=PA417&lpg=PA417&dq=hive++externalLob&source=bl&ots=VzIQN09WY9&sig=M_tZ29dRn4QU4slJPCxKPN6Qal0&hl=en&sa=X&ved=0CD4Q6AEwBGoVChMI35_TvoyDxwIVRo4sCh2MQwLY#v=onepage&q=hive%20%20externalLob&f=false
--- scripts/import.sh
+++ scripts/import.sh
@@ -12,10 +12,70 @@ your_db_pwd="$7"
 hivedbname="$8"
 
 dbname="$9"
-tablename="${10}"
-mapcolumn="${11}"
+tablename="${10,,}" # to lower-case
+javamapcolumn="${11}"
 splitby="${12}"
 conditions="${13}"
+
+###
+# Split the comma-separated java-typed columns and constructs Hive-typed equivalent, if necessary
+###
+function convertJavaToHiveType() {
+   # split on commas
+   local columns=""
+   IFS=',' read -ra columns <<< "$1"
+
+   convertJavaToHiveType_result=""
+
+   for col in "${columns[@]}"; do
+
+       case "$col" in
+
+          *Integer)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=INT"
+            ;;
+          *Long)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=LONG"
+            ;;
+          *Float)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=FLOAT"
+            ;;
+          *Double)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=DOUBLE"
+            ;;
+          *Boolean)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=BOOLEAN"
+            ;;
+          *String)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=STRING"
+            ;;
+          # All date types are treated the same
+          *Date)
+            ;&
+          *Time)
+            ;&
+          *Timestamp)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=TIMESTAMP"
+            ;;
+          *BigDecimal)
+            convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=DECIMAL"
+            ;;
+          # BLOBs are treated the same
+          *ClobRef)
+             ;&
+          *BlobRef)
+             # Keep the column name, replace data type with '=BINARY' (variable expansion) and append
+             convertJavaToHiveType_result="${convertJavaToHiveType_result},${col%=*}=BINARY"
+             ;;
+          *)
+             # Other cases, do nothing
+             ;;
+       esac
+   done
+
+   # return and get rid of leading comma
+   echo "${convertJavaToHiveType_result:1}"
+}
 
 
 export HIVE_CONF_DIR="${hive_conf_dir}"
@@ -37,14 +97,22 @@ fi
 
 SQOOP_OPTIONS+=(--query \""select * from ${tablename} WHERE ${conditions} AND \$CONDITIONS"\")
 
+SQOOP_OPTIONS+=(--inline-lob-limit \"0\")
+
 if [ "${splitby}" != "" ]; then
   SQOOP_OPTIONS+=(--split-by \""${splitby}"\")
 else
   SQOOP_OPTIONS+=(--num-mappers \"1\")
 fi
 
-if [ "${mapcolumn}" != "" ]; then
-  SQOOP_OPTIONS+=(--map-column-java \""${mapcolumn}"\" --map-column-hive \""${mapcolumn}"\")
+if [ "${javamapcolumn}" != "" ]; then
+  SQOOP_OPTIONS+=(--map-column-java \""${javamapcolumn}"\")
+
+  hivemapcolumn="$(convertJavaToHiveType "${javamapcolumn}")"
+
+  if [ "${hivemapcolumn}" != '' ]; then
+      SQOOP_OPTIONS+=(--map-column-hive \""${hivemapcolumn}"\")
+  fi
 fi
 
 SQOOP_OPTIONS+=(--hive-table \""${hivedbname}.${tablename}"\")
